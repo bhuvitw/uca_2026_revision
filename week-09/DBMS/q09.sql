21d48386aef9c15de1b508bf73e882b7ff0ef4ba
Bhuvnesh <95975529+bhuvitw@users.noreply.github.com>
2026-09-26 08:07:33 +0530

    Solved q09.sql

--- week-09/DBMS/q09.sql
+++ week-09/DBMS/q09.sql
@@ -1,2 +1,16 @@
-Find the department(s) where the total fees collected (summing Fee_Payment.amount from students who belong to that department) is more than 20% of the department's budget. Show department name, total collected, budget, and the percentage.
+-- , Student(student_id, name, dept_id, admission_year, email)
+-- , Department(dept_id, dept_name, building, budget)
+-- , Fee_Payment(payment_id, student_id, amount, payment_date, semester)
+
+
+-- Find the department(s) where the total fees collected (summing Fee_Payment.amount from students who belong to that department) is more than 20% of the department's budget. Show department name, total collected, budget, and the percentage.
+
+SELECT
+d.dept_name,SUM(f.amount)as total_collected, d.budget, SUM(f.amount) * 100 / d.budget as percentage 
+FROM Department d
+JOIN Student s ON s.dept_id = d.dept_id
+JOIN Fee_Payment f ON f.student_id = s.student_id
+GROUP BY d.dept_id, d.dept_name, d.budget
+HAVING SUM(amount) > .20 * d.budget
+
 
