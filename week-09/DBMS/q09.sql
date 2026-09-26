a8e15b1de3e712f5c1835feb9207db0de64ea7a3
Bhuvnesh <95975529+bhuvitw@users.noreply.github.com>
2026-09-26 07:59:23 +0530

    Create q09.sql

---
+++ week-09/DBMS/q09.sql
@@ -0,0 +1,2 @@
+Find the department(s) where the total fees collected (summing Fee_Payment.amount from students who belong to that department) is more than 20% of the department's budget. Show department name, total collected, budget, and the percentage.
+
